9166b17a1a526e3337f342bbe798a7a04b6fcf15
PankajDeshmukh1999 <pankajdeshmukh1999@gmail.com>
2021-12-06 17:49:44 +0530

    jdbc connect with DB

---
+++ src/jdbc/JdbcDemo.java
@@ -0,0 +1,25 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcDemo {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");    //registering driver
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pankaj","Pankaj","Pankajd@1999");
+
+            Statement statement = connection.createStatement();
+            String insert = "insert into employee(id,address,name) values (1,'pune','rahul')";
+
+            int i = statement.executeUpdate(insert);
+
+            System.out.println("row inserted-> "+i);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
