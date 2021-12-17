bdb6fa4a89ae470c1cc6707d3ed187654239d238
PankajDeshmukh1999 <pankajdeshmukh1999@gmail.com>
2021-12-17 16:48:44 +0530

    Local DateTime

--- src/java8/DefaultAndStatic.java
+++ src/java8/DefaultAndStatic.java
@@ -1,41 +1,36 @@
 package java8;
 
-public class DefaultAndStatic implements MyInterface6,MyInterface7 {
+public class DefaultAndStatic implements MyInterface6 ,MyInterface7{
     public static void main(String[] args) {
         DefaultAndStatic obj = new DefaultAndStatic();
-        System.out.println(obj.add(3,6,4));
+
+        System.out.println(obj.msg());
+        System.out.println(obj.add(5,6,9));
         System.out.println(MyInterface6.display());
 
     }
 
     @Override
     public String msg() {
-        return "msg";
+        return "hi";
     }
 
     @Override
     public int add(int i, int j,int k) {
-        return MyInterface6.super.add(i,j);
+        return MyInterface7.super.add(i, j , k);
     }
 }
 interface MyInterface6{
     String msg();
 
-    private void sub(){
-        System.out.println("In sub method");
-    }
-
-    default int add(int i, int j){  // access using class object name
-        sub();
+    default int add(int i, int j){
         return i+j;
     }
-    static String display(){    // we can access directly by using interface name.method name
-       return "In display method";
+    static String display(){
+        return "In static method";
     }
 
 }
-
-
 interface MyInterface7{
     default int add(int i, int j, int k){
         return i+j+k;
--- src/java8/MethodRefToConstructor.java
+++ src/java8/MethodRefToConstructor.java
@@ -4,16 +4,13 @@ public class MethodRefToConstructor {
     public static void main(String[] args) {
 
         //refer
-        MyInterface5 myInterface5 = MethodRefToConstructor::new; //interface-obj-class name-new
-        //call
+        MyInterface5 myInterface5 = MethodRefToConstructor :: new ;
         myInterface5.methodRef();
-
     }
     public MethodRefToConstructor(){
         System.out.println("In constructor");
     }
 }
-@FunctionalInterface
 interface MyInterface5{
-    MethodRefToConstructor methodRef(); // constructor or class name
+    MethodRefToConstructor methodRef();
 }
